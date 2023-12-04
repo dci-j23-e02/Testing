cb5fd4eb60d25f92031f65309883540a25795213
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-12-04 09:54:04 +0100

    init commit

--- Annotations/src/main/Calculator.java
+++ Annotations/src/main/Calculator.java
@@ -1,3 +1,11 @@
 public class Calculator {
 
+public static double add(double x, double y){
+  return x+y ;
+}
+
+
+  public static double multiply(double x, double y){
+    return x*y ;
+  }
 }
---
+++ Annotations/src/test/CalculatorTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void multiply() {
+  }
+}
