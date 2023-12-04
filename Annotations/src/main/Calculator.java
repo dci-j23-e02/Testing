82c593dd54a05df0616dbf8203dab47b883411d8
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-12-04 10:20:34 +0100

    step1 commit

--- Annotations/src/main/Calculator.java
+++ Annotations/src/main/Calculator.java
@@ -8,4 +8,10 @@ public static double add(double x, double y){
   public static double multiply(double x, double y){
     return x*y ;
   }
+
+
+  @Override
+  public String toString() {
+    return "Calculator{}";
+  }
 }
--- Annotations/src/test/CalculatorTest.java
+++ Annotations/src/test/CalculatorTest.java
@@ -1,11 +1,15 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 
   @Test
+  @DisplayName("Add two numbers")
   void add() {
+    System.out.println("**--- Test method add is executed ---**");
+    assertEquals(1, Calculator.add(0,0));
   }
 
   @Test
